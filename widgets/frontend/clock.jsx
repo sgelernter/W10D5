a325e7df10bb3af8ca8a8c95aba64caed95c2332
sam <s.gelernter@gmail.com>
2021-12-10 15:48:02 -0500

    Display date and time

--- widgets/frontend/clock.jsx
+++ widgets/frontend/clock.jsx
@@ -4,16 +4,18 @@ class Clock extends React.Component {
 
     constructor() {
         super();
-        this.state = new Date();
+        const date = new Date();
+        this.state = {date: date};
         this.tick = this.tick.bind(this);
+        // debugger
     }
 
     tick() {
-        this.setState = new Date();
-
+        const newDate = new Date();
+        this.setState({date: newDate});
     }
     componentDidMount() {
-        this.int = setInterval(tick, 1000);
+        this.int = setInterval(this.tick, 1000);
     }
     componentWillUnmount() {
         clearInterval(this.int);
@@ -22,7 +24,12 @@ class Clock extends React.Component {
     render() {
         return (
             <div className = "clock-index">
-                <h1>Real Time Clock</h1>
+                <h1>Real Time Clock</h1>   
+                <div className = "clock-time">
+                    Date: {(this.state.date).getMonth()} / {(this.state.date).getDate()} / {(this.state.date).getFullYear()}
+                    <br/>
+                    Time: {(this.state.date).getHours()} : {(this.state.date).getMinutes()} : {(this.state.date).getSeconds()}
+                </div>
             </div>
         )
     }
